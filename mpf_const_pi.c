649e122085f1f53402d5b60cfb11a9c7478bbdd3
czurnieden <czurnieden@gmx.de>
2015-03-02 00:35:52 +0100

    BUGFIX: added error handling to const_pi

--- mpf_const_pi.c
+++ mpf_const_pi.c
@@ -22,18 +22,17 @@ int mpf_const_pi(mp_float * a)
 {
     int err;
     mp_float aa, b, d, d2, s, t, p, two, twoinv;
-    long eps, oldeps, extra, k, loops,r;
+    long eps, oldeps, extra, k, loops, r;
 
     oldeps = a->radix;
     err = MP_OKAY;
 
     // Sometimes memory is short, even today
-    if(mpf_pi_precision > 0 && a == NULL){
-      mpf_clear(&mpf_pi);
-      mpf_pi_precision = 0;
-      return err;
+    if (mpf_pi_precision > 0 && a == NULL) {
+	mpf_clear(&mpf_pi);
+	mpf_pi_precision = 0;
+	return err;
     }
-
     // five percent plus 3 bit angst-allowance
     // TODO: compute correct value
     extra = (oldeps / 100) * 5 + 3;
@@ -45,92 +44,138 @@ int mpf_const_pi(mp_float * a)
     } else {
 	eps = oldeps + extra;
 	if (mpf_pi_precision == 0) {
-	    mpf_init(&mpf_pi, eps);
+	    if ((err = mpf_init(&mpf_pi, eps)) != MP_OKAY) {
+		return err;
+	    }
 	}
 
-	mpf_init_multi(eps, &aa, &b, &d, &d2, &s, &t, &p, &two, &twoinv, NULL);
+	if ((err =
+	     mpf_init_multi(eps, &aa, &b, &d, &d2, &s, &t, &p, &two, &twoinv,
+			    NULL)) != MP_OKAY) {
+	    return err;
+	}
+	// this algorithm is quadratic, produces twice the amount of digits
+	// every round, so the maximum number of rounds is log_2(radix)
+	loops = 0;
+	// if eps is negative we have a really large problem but somewhere else
+	r = eps;
+	while (r >>= 1) {
+	    loops++;
+	}
+	// ceil(log_2(radix)) + angst-allowance
+	loops += 3;
 
-        // this algorithm is quadratic, produces twice the amount of digits
-        // every round, so the maximum number of rounds is log_2(radix)
-        loops = 0;
-        // if eps is negative we have a really large problem but somewhere else
-        r = eps;
-        while (r >>= 1){
-           loops++;
-        }
-        // ceil(log_2(radix)) + angst-allowance
-        loops += 3;
-
-        /*
-            Initialize:
-            a = 1, b = 1/sqrt(2), t = 1/2, k = 1
-         */
+	/*
+	 * Initialize:
+	 * a = 1, b = 1/sqrt(2), t = 1/2, k = 1
+	 */
 	// aa = 1
-	mpf_const_d(&aa, 1);
+	if ((err = mpf_const_d(&aa, 1)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// two = 2
-	mpf_const_d(&two, 2);
+	if ((err = mpf_const_d(&two, 2)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// twoinv = 1/two
-	mpf_inv(&two, &twoinv);
+	if ((err = mpf_inv(&two, &twoinv)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// b = sqrt(two)
-	mpf_sqrt(&two, &b);
+	if ((err = mpf_sqrt(&two, &b)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// b = 1/b
-	mpf_inv(&b, &b);
+	if ((err = mpf_inv(&b, &b)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// t = twoinv
-	mpf_copy(&twoinv, &t);
+	if ((err = mpf_copy(&twoinv, &t)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	k = 1;
 
 	do {
-            /* s = 1/2 * (a + b) */
+	    /* s = 1/2 * (a + b) */
 	    // s = aa + b
-	    mpf_add(&aa, &b, &s);
+	    if ((err = mpf_add(&aa, &b, &s)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    // s *= twoinv
-	    mpf_mul(&s, &twoinv, &s);
+	    if ((err = mpf_mul(&s, &twoinv, &s)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    /* d = aa - s */
-	    mpf_sub(&aa, &s, &d);
+	    if ((err = mpf_sub(&aa, &s, &d)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    // d = d^2
-	    mpf_sqr(&d, &d);
+	    if ((err = mpf_sqr(&d, &d)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    // aa = s
-	    mpf_copy(&s, &aa);
+	    if ((err = mpf_copy(&s, &aa)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    // s = s^2
-	    mpf_sqr(&s, &s);
-            /* t = t- 2^k * d */
+	    if ((err = mpf_sqr(&s, &s)) != MP_OKAY) {
+		goto _ERR;
+	    }
+	    /* t = t- 2^k * d */
 	    // d2 = d * 2^k
-	    mpf_copy(&d, &d2);
+	    if ((err = mpf_copy(&d, &d2)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    d2.exp += k;
 	    // t = t - d2
-	    mpf_sub(&t, &d2, &t);
-            /* b = sqrt(s - d) */
+	    if ((err = mpf_sub(&t, &d2, &t)) != MP_OKAY) {
+		goto _ERR;
+	    }
+	    /* b = sqrt(s - d) */
 	    // b = s - d
-	    mpf_sub(&s, &d, &b);
+	    if ((err = mpf_sub(&s, &d, &b)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    // b = sqrt(b);
-	    mpf_sqrt(&b, &b);
+	    if ((err = mpf_sqrt(&b, &b)) != MP_OKAY) {
+		goto _ERR;
+	    }
 	    k++;
-            if(k == loops){
-              err = MP_VAL;
+	    if (k == loops) {
+		err = MP_VAL;
 #ifdef DEBUG
-              fprintf(stderr,"Pi by AGM did not converge in %ld rounds\n",k);
+		fprintf(stderr, "Pi by AGM did not converge in %ld rounds\n",
+			k);
 #endif
-              goto _ERR;
-            }
+		goto _ERR;
+	    }
 	} while (mpf_cmp(&aa, &b) != MP_EQ);
-        /*
-                  (a + b)^2
-            pi = -----------
-                    2t
-        */
+	/*
+	 *       (a + b)^2
+	 * pi = -----------
+	 *          2t
+	 */
 	// t = t * 2
 	t.exp += 1;
 	// t = 1/t
-	mpf_inv(&t, &t);
+	if ((err = mpf_inv(&t, &t)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// p = aa + b but aa = b, so p = aa * 2
 	aa.exp += 1;
 	// p = p^2
-	mpf_sqr(&aa, &p);
+	if ((err = mpf_sqr(&aa, &p)) != MP_OKAY) {
+	    goto _ERR;
+	}
 	// p = p * t
-	mpf_mul(&p, &t, &mpf_pi);
+	if ((err = mpf_mul(&p, &t, &mpf_pi)) != MP_OKAY) {
+	    goto _ERR;
+	}
 
-	mpf_copy(&mpf_pi, a);
-        mpf_pi_precision = oldeps;
+	if ((err = mpf_copy(&mpf_pi, a)) != MP_OKAY) {
+	    goto _ERR;
+	}
+	mpf_pi_precision = oldeps;
 	return mpf_normalize_to(a, oldeps);
     }
   _ERR:
