c9dded5cbf492d857abf50e4534980ce1010d8f6
czurnieden <czurnieden@gmx.de>
2015-03-05 23:19:00 +0100

    ADDED: acos() and asin() by way of atan()

--- mpf_acos.c
+++ mpf_acos.c
@@ -1,17 +1,52 @@
-/* LibTomFloat, multiple-precision floating-point library
- *
- * LibTomFloat is a library that provides multiple-precision
- * floating-point artihmetic as well as trigonometric functionality.
- *
- * This library requires the public domain LibTomMath to be installed.
- * 
- * This library is free for all purposes without any express
- * gurantee it works
- *
- * Tom St Denis, tomstdenis@iahu.ca, http://float.libtomcrypt.org
- */
 #include <tomfloat.h>
 
-int  mpf_acos(mp_float *a, mp_float *b)
+/* 
+                    (  sqrt(1 -x^2)  )
+    acos(x) = 2 atan( -------------- )
+                    (     1 + x      )
+
+*/
+
+int mpf_acos(mp_float * a, mp_float * b)
 {
+    int err;
+    mp_float one, t;
+
+    err = MP_OKAY;
+    if ((err = mpf_init_multi(a->radix, &one, &t, NULL)) != MP_OKAY) {
+	return err;
+    }
+
+    if ((err = mpf_const_d(&one, 1)) != MP_OKAY) {
+	goto _ERR;
+    }
+
+    if ((err = mpf_sqr(a, &t)) != MP_OKAY) {
+	goto _ERR;
+    }
+    if ((err = mpf_sub(&one, &t, &t)) != MP_OKAY) {
+	goto _ERR;
+    }
+    if ((err = mpf_sqrt(&t, &t)) != MP_OKAY) {
+	goto _ERR;
+    }
+
+    if ((err = mpf_add(&one, a, &one)) != MP_OKAY) {
+	goto _ERR;
+    }
+
+    if ((err = mpf_div(&t, &one, &t)) != MP_OKAY) {
+	goto _ERR;
+    }
+
+    if ((err = mpf_atan(&t, b)) != MP_OKAY) {
+	goto _ERR;
+    }
+
+    b->exp += 1;
+
+  _ERR:
+    mpf_clear_multi(&one, &t, NULL);
+    return err;
 }
+
