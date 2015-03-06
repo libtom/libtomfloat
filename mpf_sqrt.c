77e55ce1dbc8754de217620ced0a7b6034032581
czurnieden <czurnieden@gmx.de>
2015-03-06 01:19:50 +0100

    BUGFIX: corrected early break in sqrt() loop but needs a rewrite

--- mpf_sqrt.c
+++ mpf_sqrt.c
@@ -13,47 +13,72 @@
 #include <tomfloat.h>
 
 /* compute using sqrt(x) = y_{n+1} = 0.5 * (y_{n} + x/y_{n}) */
-int  mpf_sqrt(mp_float *a, mp_float *b)
+int mpf_sqrt(mp_float * a, mp_float * b)
 {
-   mp_float oldval, tmp, res;
-   int      err, itts;
+    mp_float oldval, tmp, res, EPS, diff;
+    int err, itts;
 
-   /* make sure it's positive */
-   if (a->mantissa.sign == MP_NEG) {
-      return MP_VAL;
-   }
+    /* make sure it's positive */
+    if (a->mantissa.sign == MP_NEG) {
+	return MP_VAL;
+    }
 
-   /* let's roll */
-   if ((err = mpf_init_multi(b->radix, &oldval, &tmp, &res, NULL)) != MP_OKAY) {
-      return err;
-   }
+    /* let's roll */
+    if ((err =
+	 mpf_init_multi(b->radix, &oldval, &tmp, &res, &EPS, &diff,
+			NULL)) != MP_OKAY) {
+	return err;
+    }
 
-   /* get copy of a and make reasonable guestimte towards the sqrt */
-   if ((err = mpf_copy(a, &res)) != MP_OKAY)                                     { goto __ERR; }
-   mp_rshd(&(res.mantissa), res.mantissa.used >> 1);
-   res.exp   = res.exp / 2;
-   if ((err = mpf_normalize_to(&res, b->radix)) != MP_OKAY)                      { goto __ERR; }
+    /* get copy of a and make reasonable guestimte towards the sqrt */
+    if ((err = mpf_copy(a, &res)) != MP_OKAY) {
+	goto __ERR;
+    }
+    mp_rshd(&(res.mantissa), res.mantissa.used >> 1);
+    res.exp = res.exp / 2;
+    if ((err = mpf_normalize_to(&res, b->radix)) != MP_OKAY) {
+	goto __ERR;
+    }
 
-   /* number of iterations */
-   itts = mpf_iterations(b);
+    if ((err = mpf_const_eps(&EPS)) != MP_OKAY) {
+	goto __ERR;
+    }
 
-   while (itts--) {
-        if ((err = mpf_copy(&res, &oldval)) != MP_OKAY)                          { goto __ERR; }
+    /* number of iterations */
+    itts = mpf_iterations(b);
 
-        /* compute x/res */
-        if ((err = mpf_div(a, &res, &tmp)) != MP_OKAY)                           { goto __ERR; }
-        /* res + x/res */
-        if ((err = mpf_add(&res, &tmp, &res)) != MP_OKAY)                        { goto __ERR; }
-        /* 0.5 * (res + x/res) */
-        if ((err = mpf_div_2(&res, &res)) != MP_OKAY)                            { goto __ERR; }
+    while (itts--) {
+	if ((err = mpf_copy(&oldval, &diff)) != MP_OKAY) {
+	    goto __ERR;
+	}
+	if ((err = mpf_copy(&res, &oldval)) != MP_OKAY) {
+	    goto __ERR;
+	}
 
-        if (mpf_cmp(&res, &oldval) == MP_EQ) {
-           break;
-        }
-   }
+	/* compute x/res */
+	if ((err = mpf_div(a, &res, &tmp)) != MP_OKAY) {
+	    goto __ERR;
+	}
+	/* res + x/res */
+	if ((err = mpf_add(&res, &tmp, &res)) != MP_OKAY) {
+	    goto __ERR;
+	}
+	/* 0.5 * (res + x/res) */
+	if ((err = mpf_div_2(&res, &res)) != MP_OKAY) {
+	    goto __ERR;
+	}
 
-   mpf_exch(&res, b);
-__ERR: mpf_clear_multi(&oldval, &tmp, &res, NULL);
-   return err;
+	if ((err = mpf_sub(&res, &oldval, &oldval)) != MP_OKAY) {
+	    goto __ERR;
+	}
+	oldval.mantissa.sign = MP_ZPOS;
+	if (mpf_cmp(&oldval, &diff) == MP_EQ || mpf_cmp(&oldval, &EPS) != MP_GT) {
+	    break;
+	}
+    }
+
+    mpf_exch(&res, b);
+  __ERR:mpf_clear_multi(&oldval, &tmp, &res, &EPS, &diff, NULL);
+    return err;
 }
 
