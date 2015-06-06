cff2ef4bdb7ef9a9b230360beac1fedbead8b259
czurnieden <czurnieden@gmx.de>
2015-06-06 23:32:20 +0200

    CHANGED: but will most probably get dropped

--- mpf_getdecimalprecision.c
+++ mpf_getdecimalprecision.c
@@ -5,5 +5,5 @@ long mpf_getdecimalprecision(long radix)
     double log210 = 3.321928094887362347870319429489390175865;
     long bits;
     bits = radix;
-    return (floor(bits / log210) +1);
+    return (lround(bits / log210) +1);
 }
